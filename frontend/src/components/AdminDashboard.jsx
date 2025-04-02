2cc6d8300c69f5941e1eae3a0b5d63d09b83caac
codingjavi <pacheco.javi01@gmail.com>
2025-04-01 20:19:22 -0500

    Added Profile Seach Component

--- frontend/src/components/AdminDashboard.jsx
+++ frontend/src/components/AdminDashboard.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import HeaderBar from "./HeaderBar";
 import AuthRequest from "./AuthRequest"
 import UnassignedRoles from "./UnassignedRoles"
+import ProfileSearch from "./ProfileSearch";
 
 const AdminDashboard = () => {
   return (
     
     <div>
       <HeaderBar />
+      <main className="leftPanel">
+        <ProfileSearch />
+      </main>
       <main className="rightPanel">
         <AuthRequest />
         <UnassignedRoles />
